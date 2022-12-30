3235fa75c7c10dcc6784099e31d4e36d1e118ef3
YuryKoltashev <yurkawow@gmail.com>
2022-12-30 21:07:01 +0800

    Add data layer

---
+++ app/src/main/java/com/example/shoppinglist/data/ShopListRepositoryImpl.kt
@@ -0,0 +1,39 @@
+package com.example.shoppinglist.data
+
+import com.example.shoppinglist.domain.ShopItem
+import com.example.shoppinglist.domain.ShopListRepository
+import java.lang.RuntimeException
+
+class ShopListRepositoryImpl: ShopListRepository {
+
+    private val shopList = mutableListOf<ShopItem>()
+    private var autoIncrementId = 0
+
+    override fun addShopItem(shopItem: ShopItem) {
+        if (shopItem.id == ShopItem.UNDEFINED_ID) {
+            shopItem.id = autoIncrementId++
+        }
+        shopList.add(shopItem)
+    }
+
+    override fun editShopItem(shopItem: ShopItem) {
+        val oldShopItem = getShopItem(shopItem.id)
+        removeShopItem(oldShopItem)
+        addShopItem(shopItem)
+    }
+
+    override fun getShopItem(id: Int): ShopItem {
+        return shopList.find {
+            it.id == id
+        } ?: throw RuntimeException("Element with id: $id not found!")
+    }
+
+
+    override fun getShopList(): List<ShopItem> {
+        return shopList
+    }
+
+    override fun removeShopItem(shopItem: ShopItem) {
+        shopList.remove(shopItem)
+    }
+}
