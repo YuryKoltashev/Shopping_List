a6d1e48d5be1499ed95c8ebd12c050261a4c5d9a
YuryKoltashev <yurkawow@gmail.com>
2023-01-19 21:28:42 +0800

    Create activity_main.xml in portrait mode Add to activity_main.xml (land) vertical Guideline and FragmentContainerView Attach ShopItemFragment.kt to MainActivity.kt (via checking whether portrait mode launched or not through shopItemContainer variable) In MainActivity.kt added fragment to BackStack

--- app/src/main/java/com/example/shoppinglist/presentation/ShopItemActivity.kt
+++ app/src/main/java/com/example/shoppinglist/presentation/ShopItemActivity.kt
@@ -4,27 +4,12 @@ import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.text.Editable
-import android.text.TextWatcher
-import android.widget.Button
-import androidx.lifecycle.ViewModelProvider
 import com.example.shoppinglist.R
 import com.example.shoppinglist.domain.ShopItem
-import com.google.android.material.textfield.TextInputEditText
-import com.google.android.material.textfield.TextInputLayout
 
 class ShopItemActivity : AppCompatActivity() {
 
-    //    private lateinit var viewModel: ShopItemViewModel
-//
-//    private lateinit var til_name: TextInputLayout
-//    private lateinit var til_count: TextInputLayout
-//
-//    private lateinit var et_name: TextInputEditText
-//    private lateinit var et_count: TextInputEditText
-//
-//    private lateinit var button_Save: Button
-//
+
     private var modeType = MODE_UNKNOWN
     private var shopItemId = ShopItem.UNDEFINED_ID
 
@@ -33,63 +18,11 @@ class ShopItemActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_shop_item)
         parseIntent()
-//        viewModel = ViewModelProvider(this)[ShopItemViewModel::class.java]
-//        initViews()
-//        addTextChangeListeners()
-        setupModeType()
-//        observeViewModel()
+        if (savedInstanceState == null) {
+            setupModeType()
+        }
     }
 
-    //    private fun addTextChangeListeners() {
-//        et_name.addTextChangedListener(object : TextWatcher {
-//            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
-//            }
-//
-//            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
-//                viewModel.resetErrorInputName()
-//            }
-//
-//            override fun afterTextChanged(p0: Editable?) {
-//            }
-//        })
-//
-//        et_count.addTextChangedListener(object : TextWatcher {
-//            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
-//            }
-//
-//            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
-//                viewModel.resetErrorInputCount()
-//            }
-//
-//            override fun afterTextChanged(p0: Editable?) {
-//            }
-//        })
-//    }
-//
-//    private fun observeViewModel() {
-//        viewModel.errorInputName.observe(this) {
-//            val message = if (it) {
-//                getString(R.string.errorInputName)
-//            } else {
-//                null
-//            }
-//            til_name.error = message
-//        }
-//
-//        viewModel.errorInputCount.observe(this) {
-//            val message = if (it) {
-//                getString(R.string.errorInputCount)
-//            } else {
-//                null
-//            }
-//            til_count.error = message
-//        }
-//
-//        viewModel.shouldFinishActivity.observe(this) {
-//            finish()
-//        }
-//    }
-//
     private fun setupModeType() {
         val fragment = when (modeType) {
             MODE_ADD -> ShopItemFragment.newInstanceAddItem()
@@ -97,28 +30,10 @@ class ShopItemActivity : AppCompatActivity() {
             else -> throw RuntimeException("Unknown type of mode: $modeType")
         }
         supportFragmentManager.beginTransaction()
-            .add(R.id.shop_item_container, fragment)
+            .replace(R.id.shop_item_container, fragment)
             .commit()
     }
 
-    //
-//    private fun launchEditMode() {
-//        viewModel.getShopItem(shopItemId)
-//        viewModel.shopItem.observe(this) {
-//            et_name.setText(it.name)
-//            et_count.setText(it.count.toString())
-//        }
-//        button_Save.setOnClickListener {
-//            viewModel.editShopItem(et_name.text.toString(), et_count.text.toString())
-//        }
-//    }
-//
-//    private fun launchAddMode() {
-//        button_Save.setOnClickListener {
-//            viewModel.addShopItem(et_name.text.toString(), et_count.text.toString())
-//        }
-//    }
-//
     private fun parseIntent() {
         if (!intent.hasExtra(EXTRA_MODE_TYPE)) {
             throw RuntimeException("Mode type is absent")
@@ -135,14 +50,6 @@ class ShopItemActivity : AppCompatActivity() {
             shopItemId = intent.getIntExtra(EXTRA_SHOP_ITEM_ID, ShopItem.UNDEFINED_ID)
         }
     }
-//
-//    private fun initViews() {
-//        til_name = findViewById(R.id.til_name)
-//        til_count = findViewById(R.id.til_count)
-//        et_name = findViewById(R.id.et_name)
-//        et_count = findViewById(R.id.et_count)
-//        button_Save = findViewById(R.id.button_Save)
-//    }
 
     companion object {
 
